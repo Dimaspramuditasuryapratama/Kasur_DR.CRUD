005f01dc2bd2ac5e3e91e241e55bf26ce3730309
Dimaspramuditasuryapratama <dimaspramuditas.pgmail.com>
2026-04-14 10:46:09 +0700

    membuat constructor

--- CRUDMahasiswaADO/Form1.cs
+++ CRUDMahasiswaADO/Form1.cs
@@ -19,30 +19,7 @@ namespace CRUDMahasiswaADO
             conn = new SqlConnection(connectionString);
         }
 
-        private void ConnectDatabase()
-        {
-            try
-            { if (conn.State == System.Data.ConnectionState.Closed)
-                {
-                     conn.Open();
-                }
-                MessageBox.Show("Koneksi berhasil!");
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Koneksi gagal: " + ex.Message);
-            }
-        }
-
-        private void btnConnect_Click_1(object sender, EventArgs e)
-        {
-            ConnectDatabase();
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
+        
 
         
     }
