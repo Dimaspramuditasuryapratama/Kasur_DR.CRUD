15fb21f4a3c0ebdc3f0e40b2dca0e95f78b5fb6a
Dimaspramuditasuryapratama <dimaspramuditas.pgmail.com>
2026-06-22 22:00:33 +0700

    "feat(dal): Add connection string configuration and GetConnectionString method"

---
+++ CRUDMahasiswaADO/DAL.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Windows.Forms;
+
+namespace CRUDMahasiswaADO
+{
+    internal class DAL
+    {
+
+        static string connectionString = "Data Source=LAPTOP-2QET043V\\DIMAS;Initial Catalog=DBAkademikADO;User ID=sa;Password=Slamet454";
+
+        public string GetConnectionString()
+        {
+            
+            return connectionString;
+        }
+
+    }
+}
